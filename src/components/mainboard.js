63b1c832c3bf9e76e102f06bf6d5f55cce9211e2
supremacy1 <andrewohejiedeogbu@yahoo.com>
2023-10-02 00:53:29 +0100

    firstpush

---
+++ src/components/mainboard.js
@@ -0,0 +1,70 @@
+import React from "react";
+import mainbar from '../css/mainbar.css'
+import logo from '../images/logo.svg'
+import her from '../images/her.svg'
+import loca from '../images/loca.svg'
+import ser from '../images/ser.svg'
+import images from '../images/images.jpg'
+import download from '../images/download .png'
+
+
+const MainBoard = () => {
+
+
+
+
+    return(
+        <>
+        <div className="content">
+            <div className="head">
+           <img src={logo} className="logo" alt="logo"></img> <h1>LaslesVPN</h1>
+           </div>
+           <div className="navbar">
+           
+      <ul className="nav-list">
+        <li className="nav-item">Home</li>
+        <li className="nav-item">About</li>
+        <li className="nav-item">Services</li>
+        <li className="nav-item">Contact</li>
+        <li className="nav-item">Sign in</li>
+        <li className="nav-item">Sign up</li>
+      </ul>
+      
+    </div>
+    <div className="buttons">
+            <button type="text" className="b1">Sign in</button>
+            <button type="text" className="b2">Sign Up</button>
+            </div>
+            </div>
+            <div className="second">
+                <h1>Want anything to be easy with LaslesVPN.</h1>
+            <h5>Provide a network for all your needs with ease and fun using LaslesVPN discover interesting features from us.</h5>
+    <img src={download} className="sond"></img>
+            </div>
+            <div className="but">
+                <button className="bb">Get Started</button>
+            </div><br></br>
+        <div className="users">
+           <img src={her} className="user"></img> <h6>90+<br></br> <h4>Users</h4></h6>
+           <img src={loca} className="user"></img><h6>30+<br></br> <h4>Locations</h4></h6>
+           <img src={ser} className="user"></img><h6>50+ <br></br><h4>Servers</h4></h6>
+        </div>
+       <div className="plat">
+        <img src={images} className="hot"></img>
+        <div className="text">
+            <h2>We Provide Many Features You Can Use</h2>
+            <h4>You can explore the features that we provide with fun and have their own functions each feature.</h4>
+           <ul>
+            <li>Powerfull online protection.</li>
+            <li>Internet without borders.</li>
+            <li>Supercharged VPN</li>
+            <li>No specific time limits.</li>
+           </ul>
+
+        </div>
+       </div>
+        </>
+    )
+}
+
+export default MainBoard;
---
+++ src/components/middle.js
@@ -0,0 +1,87 @@
+import React from "react";
+import middle from '../css/middle.css'
+import Premium from '../images/Premium.svg'
+
+
+const Middle = () => {
+
+
+
+
+
+    return(
+
+        <>
+        <div className="maincontainer">
+        <h1>Choose Your Plan</h1>
+        <h5>Let's choose the package that is best for you and explore it happily and cheerfully.</h5>
+        </div>
+       
+        <div className="class">
+            <div className="class1">
+        <div className="anyher">
+            <img src={Premium} className="pp"></img>
+            
+            <h4>Free Plan</h4>
+            <ul>
+                <li>Unlimited Bandwitch</li>
+                <li>Encrypted Connection</li>
+                <li>No Traffic Logs</li>
+                <li>Works on All Devices</li>
+            </ul>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>   
+            <h4>Free Plan</h4>
+            <button>Select</button>
+            </div>
+           </div>
+           <div className="class2">
+        <div className="anyher">
+            <img src={Premium} className="pp"></img>
+            
+            <h4>Standard Plan</h4>
+            <ul>
+                <li>Unlimited Bandwitch</li>
+                <li>Encrypted Connection</li>
+                <li>Yes Traffic Logs</li>
+                <li>Works on All Devices</li>
+                <li>Connect Anyware</li>
+            </ul><br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <h4>$9 / mo</h4>
+            <button>Select</button>
+            </div>
+           </div>
+           <div className="class3">
+        <div className="anyher">
+            <img src={Premium} className="pp"></img>
+            
+            <h4>Premium Plan</h4>
+            <ul>
+                <li>Unlimited Bandwitch</li>
+                <li>Encrypted Connection</li>
+                <li>Yes Traffic Logs</li>
+                <li>Works on All Devices</li>
+                <li>Connect Anyware</li>
+                <li>Get New Features</li>
+            </ul>
+            <h4>$12 / mo</h4>
+            <button>Select</button>
+            </div>
+           </div>
+            </div>
+      
+        </>
+    )
+}
+export default Middle;
