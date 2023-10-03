a1d956b556d02722a775524dd196ede5286b53db
supremacy1 <andrewohejiedeogbu@yahoo.com>
2023-10-03 09:02:06 +0100

    goy

--- src/components/mainboard.js
+++ src/components/mainboard.js
@@ -6,6 +6,7 @@ import loca from '../images/loca.svg'
 import ser from '../images/ser.svg'
 import images from '../images/images.jpg'
 import download from '../images/download .png'
+import bx from '../images/bx.svg'
 
 
 const MainBoard = () => {
@@ -40,17 +41,38 @@ const MainBoard = () => {
             </div>
             </div>
             <div className="second">
-                <h1>Want anything to be easy with LaslesVPN.</h1>
-            <h5>Provide a network for all your needs with ease and fun using LaslesVPN discover interesting features from us.</h5>
+                <h1>Want anything to be easy with <span>LaslesVPN.</span>
+                <span><h5>Provide a network for all your needs with ease and fun using LaslesVPN discover interesting features from us.</h5></span>
+                </h1>          
     <img src={download} className="sond"></img>
             </div>
             <div className="but">
                 <button className="bb">Get Started</button>
             </div><br></br>
         <div className="users">
-           <img src={her} className="user"></img> <h6>90+<br></br> <h4>Users</h4></h6>
+        <div className="container">
+      <img src={her} alt="Image" className="image" />
+      <div className="text">
+        <h2>50+<br></br><span>Users</span></h2>
+        
+      </div>
+      </div>
+      <div className="container">
+      <img src={loca} alt="Image" className="image" />
+      <div className="text">
+        <h2>30+<br></br><span>Locations</span></h2>
+      </div>
+      </div>
+      <div className="container">
+      <img src={ser} alt="Image" className="image" />
+      <div className="text">
+        <h2>50+ <br></br><span>Server</span></h2>
+       
+      </div>
+      </div>
+           {/* <img src={her} className="user"></img> <h6>90+<br></br> <h4>Users</h4></h6>
            <img src={loca} className="user"></img><h6>30+<br></br> <h4>Locations</h4></h6>
-           <img src={ser} className="user"></img><h6>50+ <br></br><h4>Servers</h4></h6>
+           <img src={ser} className="user"></img><h6>50+ <br></br><h4>Servers</h4></h6> */}
         </div>
        <div className="plat">
         <img src={images} className="hot"></img>
@@ -58,10 +80,10 @@ const MainBoard = () => {
             <h2>We Provide Many Features You Can Use</h2>
             <h4>You can explore the features that we provide with fun and have their own functions each feature.</h4>
            <ul>
-            <li>Powerfull online protection.</li>
-            <li>Internet without borders.</li>
-            <li>Supercharged VPN</li>
-            <li>No specific time limits.</li>
+           <li><img src={bx} alt="Image" className="ima" />Powerfull online protection.</li>
+            <li><img src={bx} alt="Image" className="ima" />Internet without borders.</li>
+            <li><img src={bx} alt="Image" className="ima" />Supercharged VPN</li>
+            <li><img src={bx} alt="Image" className="ima" />No specific time limits.</li>
            </ul>
 
         </div>
