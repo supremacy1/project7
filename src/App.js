ed05962134e867156219fd95011e232329ef468b
supremacy1 <andrewohejiedeogbu@yahoo.com>
2023-10-07 13:28:13 +0100

    silde

--- src/App.js
+++ src/App.js
@@ -4,9 +4,11 @@ import Footer from './components/foot';
 import MainBoard from './components/mainboard';
 import Middle from './components/middle';
 
+
 function App() {
   return (
     <div className="App">
+      {/* <Test /> */}
      <MainBoard />
      <Middle />
      <Footer />
---
+++ src/components/test.js
@@ -0,0 +1,18 @@
+import React from "react";
+import test from '../css/test.css'
+import ima from '../images/imas.jpg'
+
+const Test = () =>{
+
+
+
+    return(
+        <>
+        <div className="got">
+        <h2>the real</h2>
+ <img src={ima} className="hot"></img>
+ </div>
+ </>
+    )
+}
+export default Test;
