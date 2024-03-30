f1a5c61cd30d848879edb80024644b41c084eec1
Andy <andrewohejiedeogbu@yahoo.com>
2024-03-30 21:08:34 +0100

    resize

--- src/App.js
+++ src/App.js
@@ -10,8 +10,8 @@ function App() {
     <div className="App">
       {/* <Test /> */}
      <MainBoard />
-     <Middle />
-     <Footer />
+     {/* <Middle />
+     <Footer /> */}
     </div>
   );
 }
